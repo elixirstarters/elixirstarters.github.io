1e51a61d7a3610fe00cc32ee93ac1d7d5fc0b046
Ile Eftimov <ileeftimov@gmail.com>
2015-10-24 00:55:49 +0200

    add various stuff

--- app.js
+++ app.js
@@ -12,5 +12,14 @@ angular.module('exstarter', [])
       console.log(err);
     });
 
+  $scope.repoName = function(html_url) {
+    parts = html_url.split("/");
+    return parts[3] + "/" + parts[4];
+  };
+
+  $scope.repoUrl = function(html_url) {
+    return "https://github.com/" + $scope.repoName(html_url);
+  };
+
 }]);
     
