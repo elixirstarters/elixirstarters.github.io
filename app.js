02f429f37e8f967650ffc3ee7afba8fd9db20f6b
Ile Eftimov <ileeftimov@gmail.com>
2015-10-23 22:00:09 +0200

    style it up!

---
+++ app.js
@@ -0,0 +1,16 @@
+angular.module('exstarter', [])
+
+.value("ghEndpoint", "https://api.github.com/search/issues?q=label:level:starter+language:elixir+state:open")
+.controller("IssuesListController", ["$scope", "$http", "ghEndpoint", function($scope, $http, ghEndpoint){
+  $http.get(ghEndpoint)
+    .success(function(response) {
+      $scope.total_issues = response.items.length;
+      $scope.issues = response.items;
+      console.log($scope.issues);
+    })
+    .error(function(err) {
+      console.log(err);
+    });
+
+}]);
+    
