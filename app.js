af44a90bab3c1d08f3bc4490f16c357a6aab461a
Ile Eftimov <ileeftimov@gmail.com>
2015-10-24 01:51:40 +0200

    add passed time since issue was created

--- app.js
+++ app.js
@@ -21,5 +21,40 @@ angular.module('exstarter', [])
     return "https://github.com/" + $scope.repoName(html_url);
   };
 
+  $scope.timeAgo = function(datetime) {
+    unix = Date.parse(datetime);
+    unixDate = new Date(unix);
+
+    return $scope.timeSince(unixDate);
+  };
+
+  $scope.timeSince = function(date) {
+
+    var seconds = Math.floor((new Date() - date) / 1000);
+
+    var interval = Math.floor(seconds / 31536000);
+
+    if (interval > 1) {
+        return interval + " years";
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+        return interval + " months";
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+        return interval + " days";
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+        return interval + " hours";
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+        return interval + " minutes";
+    }
+    return Math.floor(seconds) + " seconds";
+  }
+
 }]);
     
